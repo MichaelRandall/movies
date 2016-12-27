6df2663f4ebf2ceb882c7d0aa329cdc889563966
Mike <m_ran66@hotmail.com>
2016-12-27 12:02:28 -0800

    Added and tested command line capabilities for user to add or remove an item from the list, view the list of movies, or read about a single movie.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-//https://github.com/MichaelRandall/movies
 console.log('Starting app.js for movies');
+//const fs = require('fs');
+const _ = require('lodash');
 
 const movies = require('./movies.js');
 
-var movie = movies.addMovie('Movie Title');
-console.log(movie);
+var userInput = process.argv[2];
+console.log('User input: ', userInput);
+
+if(userInput === 'add') {
+  console.log('Adding a movie to the list');
+}else if (userInput === 'list') {
+  console.log('Displaying all movies in the list');
+}else if (userInput === 'read') {
+  console.log('Displaying a specific movie from the list');
+}else if (userInput === 'remove'){
+  console.log('Removing a specific movie from the list');
+}else{
+  console.log('User input not found');
+}
