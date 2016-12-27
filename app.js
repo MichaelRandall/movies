90fee050f3aedde8206e8ea551f62389bd10c7be
Mike <m_ran66@hotmail.com>
2016-12-27 15:59:50 -0800

    Refactored code for usability in movies.js and added method to remove a movie from the list.

--- app.js
+++ app.js
@@ -10,13 +10,20 @@ var userInput = argv._[0];
 
 
 if(userInput === 'add') {
-  movies.addMovie(argv.title,argv.description);
+  var movie = movies.addMovie(argv.title,argv.description);
+  if(movie){
+    console.log(`You got the movie title: ${movie.title}. Description: ${movie.description}`);
+  }else{
+    console.log("A duplicate value already exists");
+  }
 }else if (userInput === 'list') {
   movies.getMovies();
 }else if (userInput === 'view') {
   movies.getMovie(argv.title);
 }else if (userInput === 'remove'){
-  movies.removeMovie(argv.title);
+  var movieRemoved = movies.removeMovie(argv.title);
+  var message = movieRemoved ? 'The movie was removed' : 'The movie was not found';
+  console.log(message);
 }else{
   console.log('User input not found');
 }
--- movies.js
+++ movies.js
@@ -2,25 +2,31 @@ console.log('Starting movies.js for movies');
 
 const fs = require('fs');
 
+var fetchMovies = () => {
+  try{
+    var moviesString = fs.readFileSync('movies-list.json');
+    return JSON.parse(moviesString);
+  }catch(error){
+    return [];
+  }
+};
+
+var saveMovies = (movies) => {
+  fs.writeFileSync('movies-list.json', JSON.stringify(movies));
+};
+
 var addMovie = (title,description) => {
-  var movies = [];
+  var movies = fetchMovies();
   var movie = {
     title,
     description
   };
-
-  try{
-    var moviesString = fs.readFileSync('movies-list.json');
-    movies = JSON.parse(moviesString);
-  }catch(error){
-
-  }
-
   var duplicateMovies = movies.filter((movie) => movie.title === title);
 
   if(duplicateMovies.length === 0){
     movies.push(movie);
-    fs.writeFileSync('movies-list.json', JSON.stringify(movies));
+    saveMovies(movies);
+    return movie;
   }
 };
 
@@ -30,6 +36,11 @@ var getMovies = () => {
 
 var removeMovie = (title) => {
   console.log('Removing movie with title ' + title);
+  var movies = fetchMovies();
+  var filteredMovies = movies.filter((movie) => movie.title !== title);
+  saveMovies(filteredMovies);
+
+  return movies.length !== filteredMovies.length;
 };
 
 var getMovie = (title) => {
