0442e425ac0aaa415048bcc3cc8d565376a61b2e
Mike <m_ran66@hotmail.com>
2016-12-27 13:40:16 -0800

    Minor cosmetic changes

--- movies.js
+++ movies.js
@@ -1,10 +1,29 @@
 console.log('Starting movies.js for movies');
 
-var addMovie = () => {
+var addMovie = (title,description) => {
   //console.log('You have added a movie');
-  return 'You have added a movie';
-}
+  console.log('Adding movie', title, description);
+  //return 'You are adding a movie';
+};
 
+var getMovies = () => {
+  console.log('Retrieving all movies');
+};
+
+var removeMovie = (title) => {
+  console.log('Removing movie with title ' + title);
+};
+
+var getMovie = (title) => {
+  console.log('Getting movie with title ' + title);
+};
+
+
+
+//es6 object property and value are identical
 module.exports = {
-  addMovie
+  addMovie,
+  getMovies,
+  removeMovie,
+  getMovie
 };
